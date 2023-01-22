a1a197bb80b7cbdcbdf7254361e8b247caf0f766
Ismail <ismail.tk18@gmail.com>
2023-01-22 21:26:06 +0400

    added api calling features

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './Home';
 import News from './News-add';
+import Events from './Events';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <News />
+    <Events />
   </React.StrictMode>
 );
 
